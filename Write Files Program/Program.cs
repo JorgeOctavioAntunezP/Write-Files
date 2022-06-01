4b2fec09cf366424f6f07480046055cbfa1af849
JorgeOctavioAntunezP <jorge.oap18@gmail.com>
2022-06-01 11:17:17 -0700

    Write Files Program
    Programa 1

---
+++ Write Files Program/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Write_Files_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("Ejemplo.txt",true);
+
+            string[] Lines = { "Linea 1", "Linea 2", "Linea 3" };
+
+
+            foreach (string Line in Lines)
+            {
+                sw.WriteLine(Line);
+            }
+
+            sw.Close();
+
+            Console.WriteLine("Escribiendo . . .");
+            Console.ReadLine();
+        }
+    }
+}
